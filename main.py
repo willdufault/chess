d69b9d72467528d5254a60779263d08bb84858e4
wduf <wduf02@gmail.com>
2023-01-06 15:43:51 -0500

    minimax done, ab wip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import models
+import copy
 
 '''
 
@@ -13,6 +14,24 @@ before game:
 '''
 
 def main() -> None:
+	model = models.Model(3)
+	team = True
+	model.board.generateControlMatrix()
+	model.board.generatePositionMatrix()
+	model.board.generateLegalMoves(team)
+	print(f"eval: {model.engine.simpleEvaluate(model.board)}")
+	print(); model.board.printBoard(); print()
+
+	model.bot.bot_move(model.board, team)
+	
+	model.board.generateControlMatrix()
+	model.board.generatePositionMatrix()
+	model.board.generateLegalMoves(team)
+	print(f"eval: {model.engine.simpleEvaluate(model.board)}")
+	print(); model.board.printBoard(); print()
+
+
+	return
 	model = models.Model()
 	engine = models.Engine()
 	team = True  # white moves first
@@ -20,38 +39,38 @@ def main() -> None:
 	# game loop
 	while True:
 		# generate matrices
-		model.generateControlMatrix()
-		model.generatePositionMatrix()
-		model.generateLegalMoves(team)
-		print(model.legal_moves)
+		model.board.generateControlMatrix()
+		model.board.generatePositionMatrix()
+		model.board.generateLegalMoves(team)
+		# print(model.board.legal_moves)
 		# print eval
 		print(f"eval: {engine.simpleEvaluate(model.board)}")
 		# display board
 		print()
-		model.printBoard()
+		model.board.printBoard()
 		print()
 		# not first move
 		if last_move:
 			print("{team}: {last_move}".format(team = ("black" if team else "white"), last_move = last_move))
 		# stalemate
-		if model.checkStale(team):
+		if model.board.checkStale(team):
 			print("stalemate. it's a draw.")
 			return
 		# checkmate
-		if model.checkMate(team):
+		if model.board.checkMate(team):
 			print("checkmate. {team} wins!".format(team = ("black" if team else "white")))
 			return
 		# check
-		if model.checkCheck(team):
-			if model.checkMate(team):
+		if model.board.checkCheck(team):
+			if model.board.checkMate(team):
 				print("checkmate. {team} wins!".format(team = ("black" if team else "white")))
 			print("you're in check.\n")
 		inp = ""  # user input
 		# must be valid + legal move
-		while not (model.validMove(inp, team) and model.legalMove(inp)) :
+		while not (model.board.validMove(inp, team) and model.board.legalMove(inp)) :
 			inp = input("{team}'s move?\n".format(team = ("white" if team else "black").strip()))
 		# valid move given, move that piece
-		model.movePiece(inp)
+		model.board.movePiece(inp)
 		# update last move
 		last_move = inp
 		# next team's turn
--- models/bot.py
+++ models/bot.py
@@ -1,17 +1,94 @@
+import math
 import random
+from models.engine import Engine
 
 class Bot:
 	def __init__(self, depth: int):
 		self.depth = depth
+		# could pass bot an engine to use from model
 
-	def minimax(self, board: object, team: bool, cur_depth: int) -> int:
-		# bad practice to make a model inside of bot, p sure this is the simplest way to do this
-		# maybe model has bot and engine fields, uses those to call these functions, can just pass in legal moves to minimax?
+	def bot_move(self, board: object, team: bool) -> None:
+		moves = board.legal_moves  # all legal moves for this board and team
+		scores = []  # scores for each move
+		for i, m in enumerate(moves):
+			r, c, r1, c1 = m
+			old, old1 = board.squares[r][c], board.squares[r1][c1]
+			board.movePiece(str(r) + str(c) + str(r1) + str(c1))
 
-		# for every legal move on this board, try every legal move after every legal move -> do this depth times
-		# once at leaf, calculate best move and it's score
-		# compare that against all others, pass it up
-		# repeat until single move chosen
-		# incorporate ab later
-		pass
+			scores.append((self.minimax_ab(team, self.depth - 1, -math.inf, math.inf, board), i))
+			# undo move
+			board.squares[r][c], board.squares[r1][c1] = old, old1
+		best_score = max(scores) if team else min(scores)
+		# random choice of this, fix syntax
+		choices = [s for s in scores if (s == (max(scores) if team else min(scores)))]  # moves that have best score
+		best_move = moves[random.choice(choices)[1]]  # best_move
+		board.movePiece(str(best_move[0]) + str(best_move[1]) + str(best_move[2]) + str(best_move[3]))
 
+	def minimax(self, team: bool, depth: int, board: object) -> int:
+		'''
+		for every legal move, try it and its legal moves (depth times), find the move with the greatest outcome
+		'''
+		board.generateControlMatrix()
+		board.generatePositionMatrix()
+		board.generateLegalMoves(team)
+		# stalemate
+		if board.checkStale(team):
+			return 0
+		# checkmate
+		if board.checkMate(team):
+			#? add tuple (cur_depth, score) so M1 is picked over M3?
+			#* mate score arbitrary for now
+			return -20 if team else 20
+		# reached desired depth
+		if not depth:
+			return Engine.simpleEvaluate(Engine, board)
+		moves = board.legal_moves  # all legal moves for this board
+		scores = []  # scores for all legal moves
+		for m in moves:
+			r, c, r1, c1 = m
+			old, old1 = board.squares[r][c], board.squares[r1][c1]
+			board.movePiece(str(r) + str(c) + str(r1) + str(c1))
+			scores.append(self.minimax(not team, depth - 1, board))
+			# undo move
+			board.squares[r][c], board.squares[r1][c1] = old, old1
+		return max(scores) if team else min(scores)
+
+	def minimax_ab(self, team: bool, depth: int, a: int, b: int, board: object) -> int:
+		'''
+		for every legal move, try it and its legal moves (depth times), find the move with the greatest outcome
+		* with alpha-beta pruning to eliminate unnecessary calls
+		* to start, a (high) = -oo, b (low) = oo
+		'''
+		board.generateControlMatrix()
+		board.generatePositionMatrix()
+		board.generateLegalMoves(team)
+		# stalemate
+		if board.checkStale(team):
+			return 0
+		# checkmate
+		if board.checkMate(team):
+			#? add tuple (cur_depth, score) so M1 is picked over M3?
+			#? or add higher score if depth is higher/earlier?)
+			#* mate score arbitrary for now
+			return -20 if team else 20
+		# reached desired depth
+		if not depth:
+			return Engine.simpleEvaluate(Engine, board)
+		moves = board.legal_moves  # all legal moves for this board
+		scores = []  # scores for all legal moves
+		for m in moves:
+			r, c, r1, c1 = m
+			old, old1 = board.squares[r][c], board.squares[r1][c1]
+			board.movePiece(str(r) + str(c) + str(r1) + str(c1))
+			scores.append(score := self.minimax_ab(not team, depth - 1, a, b, board))
+			# undo move
+			board.squares[r][c], board.squares[r1][c1] = old, old1
+			# maximizing
+			if team:
+				a = max(a, score)
+			# minimizing
+			else:
+				b = min(b, score)
+			if a > b:
+				break
+		return max(scores) if team else min(scores)
